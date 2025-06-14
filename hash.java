70de762f8bce0cfd81e29ac912b89910f1df6fa1
aam487 <aam487@drexel.edu>
2025-06-14 15:26:39 -0400

    Create hash.java

---
+++ hash.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+public class hash {
+    public static void main(String[] args) {
+    
+    int max = 122;
+    int min = 97;
+    int y = 0;
+    int b = 0;
+    
+    Random ran = new Random();
+   for (int i = 0; i < 5; i++) {
+    for (y = 0; y < 10; y++){
+       b = ran.nextInt(max - min + 1) + min;
+       char character = (char) b; 
+       System.out.print(character + " ");
+    }
+    System.out.println();
+}
+
+System.out.println();
+    }
+}
+
+
+@aam487 ➜ /workspaces/Dwayne-Java (main) $  cd /workspaces/Dwayne-Java ; /usr/bin/env /usr/local/sdkman/candidates/java/21.0.6-ms/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/codespace/.vscode-remote/data/User/workspaceStorage/f97a03c/redhat.java/jdt_ws/Dwayne-Java_f5daeb/bin hash 
+k u c u i s c c v x 
+w t s i z w n k k h 
+s q m m u q m r u d 
+a l y e v w n r r h 
+y n o q t p m g t n 
