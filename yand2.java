9e777ceff11f89b9ee124e998aaba92c42181914
aam487 <aam487@drexel.edu>
2025-06-13 23:07:33 -0400

    Create yand2.java

---
+++ yand2.java
@@ -0,0 +1,28 @@
+public class yand {
+    public static <subA> void main(String[] args){
+        
+    for (int y = 65; y < y + 24; y++){
+
+        //System.out.print(y);
+        char character = (char) y;
+        System.out.print(character + " ");
+        if (y == 90){
+            break;      
+      }
+    }
+
+    System.out.println();
+
+    for (int y = 97; y < y + 24; y++){
+
+        //System.out.print(y);
+        char character = (char) y;
+        System.out.print(character + " ");
+        if (y == 97+25){
+            break;      
+      }
+    }
+
+    System.out.println();
+  }   
+}
