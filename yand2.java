b77f890efd291fd81cdcfdb7fd4bc4d4301d6d80
aam487 <aam487@drexel.edu>
2025-06-13 23:08:58 -0400

    Update yand2.java

--- yand2.java
+++ yand2.java
@@ -26,3 +26,9 @@ public class yand {
     System.out.println();
   }   
 }
+
+
+@aam487 ➜ /workspaces/Dwayne-Java (main) $  cd /workspaces/Dwayne-Java ; /usr/bin/env /usr/local/sdkman/candidates/java/21.0.6-ms/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/codespace/.vscode-remote/data/User/workspaceStorage/f97a03c/redhat.java/jdt_ws/Dwayne-Java_f5daeb/bin yand 
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+a b c d e f g h i j k l m n o p q r s t u v w x y z 
+@aam487 ➜ /workspaces/Dwayne-Java (main) $ ^C
