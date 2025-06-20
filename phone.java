a644a4f8482f65122b817b7cf4fb48608feadf26
aam487 <aam487@drexel.edu>
2025-06-19 22:25:40 -0400

    Create phone.java

---
+++ phone.java
@@ -0,0 +1,27 @@
+public class phone {
+    public static void main(String[] args) {
+        
+     for (int x = 0; x < 1; x++){
+        for (int y = 1; y < 10; y++){
+            System.out.print(y + "   ");
+            if (y == 3 || y == 6){
+                System.out.println();
+                System.out.println();
+            }
+        }
+     }
+ System.out.println();
+ System.out.println();
+ System.out.print("*   0   #");
+ System.out.println();
+    }
+}
+
+1   2   3   
+
+4   5   6   
+
+7   8   9   
+
+*   0   #
+@aam487 ➜ /workspaces/Dwayne-Java (main) $ 
