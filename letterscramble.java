4f7cb6b36b7e951b24b631052201e45a6396852a
aam487 <aam487@drexel.edu>
2025-06-19 23:09:20 -0400

    Create letterscramble.java

---
+++ letterscramble.java
@@ -0,0 +1,30 @@
+public class letterscramble {
+    public static void main(String[] args) {
+        System.out.println();
+        for (int y = 65; y < 95; y++){
+            char character = (char) y;
+            System.out.print(character + "   ");
+            if (y == 69 || y == 74 || y == 79 || y == 84 || y == 89){
+                System.out.println();
+                System.out.println();
+            }
+        }
+ System.out.println();
+ System.out.println();
+ System.out.println();
+    }
+}
+
+/Dwayne-Java_f5daeb/bin letterscramble 
+
+A   B   C   D   E   
+
+F   G   H   I   J   
+
+K   L   M   N   O   
+
+P   Q   R   S   T   
+
+U   V   W   X   Y   
+
+Z   [   \   ]   ^   
