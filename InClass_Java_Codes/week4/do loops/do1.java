88ba71cb32bce2acf7c6b4b8fd55c2da22bd6483
aam487 <aam487@drexel.edu>
2025-07-08 22:35:06 -0400

    Create do1.java

---
+++ InClass_Java_Codes/week4/do loops/do1.java
@@ -0,0 +1,33 @@
+/**********************************
+ * do1.java
+ * simple do loop that counts to 10
+ * @author Anthony Mayes
+ * @version 2.00
+ *********************************/
+public class do1 {
+
+    public static void main(String[] args) {
+    
+        int w = 1;
+        do {
+
+         
+          System.out.println(w);
+          w++;
+
+        } while (w < 11); //when w = 11 then do loop is false
+        
+    }
+
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+@aam487 ➜ /workspaces/Dwayne-Java (main) $ 
