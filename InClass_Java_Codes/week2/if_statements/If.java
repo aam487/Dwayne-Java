0e69197b27684b48bac9b59f953c812bebde9418
aam487 <aam487@drexel.edu>
2025-07-08 20:55:25 -0400

    Update If.java

--- InClass_Java_Codes/week2/if_statements/If.java
+++ InClass_Java_Codes/week2/if_statements/If.java
@@ -1,1 +1,25 @@
+/**********************************
+ * If.java
+ * If examples
+ * @author Anthony Mayes
+ * @version 2.00
+ *********************************/
+public class If {
+    public static void main(String[] args) {
 
+    //Simple if statement
+    int a = 5;
+    if (a > 0) {
+        System.out.println("a is positive");
+    }
+    //If-else statement
+    int b = -3;
+    if (b > 0) {
+        System.out.println("b is positive");
+    } else {
+        System.out.println("b is negative");
+    }
+ }
+}
+a is positive
+b is negative
