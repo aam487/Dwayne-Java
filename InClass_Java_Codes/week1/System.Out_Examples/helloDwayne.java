c484e0f55b062e8a5704596c5b0c6d6d26236be5
aam487 <aam487@drexel.edu>
2025-07-08 20:10:36 -0400

    Update helloDwayne.java

--- InClass_Java_Codes/week1/System.Out_Examples/helloDwayne.java
+++ InClass_Java_Codes/week1/System.Out_Examples/helloDwayne.java
@@ -1,1 +1,20 @@
+/**********************************
+ * helloDwayne.java
+ * System.out examples
+ * @author Anthony Mayes
+ * @version 2.00
+ *********************************/
+public class helloDwayne {
+  public static <subA> void main(String[] args) {
+
+    System.out.println("Hello, Dwayne how are you!!!"); //Prints out Hello, Dwayne how are you!!!
+    System.out.println();
+    System.out.printf("\u001B[0m");
+    System.out.println("Hello, Dwayne how are you!!!"); //Prints out Hello, Dwayne how are you!!!
+
+  }
+    
+    
+
+    
 
