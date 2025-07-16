9b4067248b068271598877c65a7ab21f0632aba6
aam487 <aam487@drexel.edu>
2025-07-15 21:14:39 -0400

    Create airplane.java

---
+++ InClass_Java_Codes/week5/Test1/airplane.java
@@ -0,0 +1,22 @@
+public class plane {
+    public static void main(String[] args) {
+        
+      System.out.println();
+      for (int x = 1; x < 4; x++) { 
+        for (int y = 1; y < 4; y++) {
+        }
+    System.out.printf("%55s", x + "A " + x +"B " + x + "C ");
+    System.out.print("  ");
+    System.out.println(x + "D " + x +"E " + x + "F ");
+    System.out.println();
+      } 
+    System.out.println("    "); 
+    } 
+}
+
+OUTPUT:
+                                              1A 1B 1C   1D 1E 1F 
+
+                                              2A 2B 2C   2D 2E 2F 
+
+                                              3A 3B 3C   3D 3E 3F 
